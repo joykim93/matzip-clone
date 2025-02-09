722315230c74b23fc4dc673d8cca4ffc966b213a
joykim93 <joykim9311@gmail.com>
2025-02-09 18:21:53 +0900

    feat: 게시글 생성 API 추가

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller()
 export class PostController {
@@ -8,5 +9,10 @@ export class PostController {
     @Get('/posts')
     async getPosts(): Promise<any> {
         return this.postService.getPosts();
+    }
+
+    @Post('/posts')
+    createPost(@Body() createPostDto: CreatePostDto ) {
+        return this.postService.createPost(createPostDto);
     }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostService {
 
+    constructor(
+        @InjectRepository(Post)
+        private postRepository: Repository<Post>,
+    ){}
+
     getPosts(): any {
         return ['a게시글', 'b게시글'];
+    }
+
+    async createPost(createPostDto: CreatePostDto) {
+        const {
+            latitude,
+            longitude,
+            color,
+            address,
+            title,
+            description,
+            date,
+            score,
+            imageUris
+        } = createPostDto
+
+        const post = this.postRepository.create({
+            latitude,
+            longitude,
+            color,
+            address,
+            title,
+            description,
+            date,
+            score,
+        })
+
+        try {
+            await this.postRepository.save(post);
+        } catch (error) {
+            console.log(error)
+            throw new InternalServerErrorException('장소를 추가하는 과정 에러가 발생했습니다.')
+        }
+        
+        return post;
     }
 }
