47f37623868ad301870c6dd1f19dc305951d6664
joykim93 <joykim9311@gmail.com>
2025-03-28 00:26:23 +0900

    feat: AWS S3 버킷에 파일 업로드 하는 방식으로 변경

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,16 +1,9 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Controller, Post, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { basename, extname } from 'path';
 import { numbers } from 'src/@common/constants';
-import * as fs from 'fs';
-
-try {
-    fs.readdirSync('uploads');
-} catch (error) {
-    fs.mkdirSync('uploads');
-}
+import { getUniqueFileName } from 'src/@common/utils';
 
 @Controller('image')
 @UseGuards(AuthGuard())
@@ -18,21 +11,34 @@ export class ImageController {
 
     @UseInterceptors(
         FilesInterceptor('images', numbers.MAX_IMAGE_COUNT, {
-            storage: diskStorage({
-                destination(req, file, cb) {
-                    cb(null, 'uploads/')
-                },
-                filename(req, file, cb) {
-                    const ext = extname(file.originalname);
-                    cb(null, basename(file.originalname, ext) + Date.now() + ext)
-                }
-            }),
             limits: { fileSize: numbers.MAX_IMAGE_SIZE },
         })
     )
     @Post('/')
-    uploadImages(@UploadedFiles() files: Express.Multer.File[]) {
-        const uris = files.map((file) => file.filename);
-        return uris;
+    async uploadImages(@UploadedFiles() files: Express.Multer.File[]) {
+        const s3Client = new S3Client({
+            region: process.env.AWS_BUCKET_REGION,
+            credentials: {
+                accessKeyId: process.env.S3_ACCESS_KEY_ID,
+                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+            }
+        })
+
+        const uuid = Date.now();
+
+        const uploadPromises = files.map((file) => {
+            const fileName = getUniqueFileName(file, uuid);
+            const uploadParams = {
+                Bucket: process.env.AWS_BUCKET_NAME,
+                Key: `original/${fileName}`,
+                Body: file.buffer,
+            }
+
+            const command = new PutObjectCommand(uploadParams)
+
+            return s3Client.send(command);
+        })
+
+        await Promise.all(uploadPromises);
     }
 }
